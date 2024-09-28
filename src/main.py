141ff68b7cdfa944505d3d80c4fb5cc5ecf41554
Felipe Sampaio <03.felipesampaio@gmail.com>
2024-09-27 21:56:12 -0300

    Write receipt result do Bigquery

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-from google.cloud import storage
+from dotenv import load_dotenv
+load_dotenv()
+
+from google.cloud import storage, bigquery
 import functions_framework
 from cloudevents.http.event import CloudEvent
 
@@ -6,10 +9,13 @@ import uber_scraper
 import json
 from base64 import urlsafe_b64decode
 
+import os
+
 
 @functions_framework.cloud_event
 def parse_email_from_file(cloud_event: CloudEvent):
     storage_client = storage.Client()
+    bigquery_client = bigquery.Client()
     bucket = storage_client.get_bucket(cloud_event.data["bucket"])
     filename = cloud_event.data["name"]
 
@@ -19,3 +25,12 @@ def parse_email_from_file(cloud_event: CloudEvent):
         uber_receipt = uber_scraper.parse_message_body(
             urlsafe_b64decode(file_content["payload"]["body"]["data"])
         )
+
+        bigquery_dataset = bigquery_client.create_dataset(os.environ["BIGQUERY_DATASET_NAME"], exists_ok=True)
+        bigquery_table = bigquery_client.create_table(os.environ['UBER_RECEIPTS_BIGQUERY_TABLE'], exists_ok=True)
+
+        errors = bigquery_client.insert_rows_json(bigquery_table, [uber_receipt])
+        if errors == []:
+            print("New rows have been added.")
+        else:
+            print("Encountered errors while inserting rows: {}".format(errors))
