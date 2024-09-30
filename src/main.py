a95a2f4697083cc64f457794bf0d2c5c73850038
Felipe Sampaio <03.felipesampaio@gmail.com>
2024-09-30 17:57:47 -0300

    Fix table creation problems

--- src/main.py
+++ src/main.py
@@ -26,10 +26,18 @@ def parse_email_from_file(cloud_event: CloudEvent):
             urlsafe_b64decode(file_content["payload"]["body"]["data"])
         )
         # Datetime.date is not json serializable. So all dates and timestamps must be converted
-        uber_receipt['date'] = str(uber_receipt['date'])
+        uber_receipt['ride_date'] = str(uber_receipt['ride_date'])
 
         bigquery_dataset = bigquery_client.create_dataset(os.environ["BIGQUERY_DATASET_NAME"], exists_ok=True)
-        bigquery_table = bigquery_client.create_table(os.environ['UBER_RECEIPTS_BIGQUERY_TABLE'], exists_ok=True)
+        bigquery_table = bigquery.Table(os.environ['UBER_RECEIPTS_BIGQUERY_TABLE'], schema=(
+            bigquery.SchemaField("ride_date", "DATE", mode="REQUIRED", description="Date of the ride"),
+            bigquery.SchemaField("ride_value", "NUMERIC", mode="REQUIRED", description="Value of the ride", precision=10, scale=2),
+            bigquery.SchemaField("stars", "NUMERIC", mode="REQUIRED", description="Driver rating", precision=3, scale=2),
+            bigquery.SchemaField("category", "STRING", mode="REQUIRED", description="Ride category (UberX, Uber Black, etc)"),
+            bigquery.SchemaField("distance", "NUMERIC", mode="REQUIRED", description="Ride distance (in kilometers)", precision=5, scale=2),
+            bigquery.SchemaField("duration", "NUMERIC", mode="REQUIRED", description="Ride duration (in minutes)", precision=5, scale=2)
+        ))
+        bigquery_table = bigquery_client.create_table(bigquery_table, exists_ok=True)
 
         errors = bigquery_client.insert_rows_json(bigquery_table, [uber_receipt])
         
